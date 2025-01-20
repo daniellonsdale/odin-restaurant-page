b9b1c0d6fb16fbf77abac591a58403295573c787
daniellonsdale <danielllonsdale@gmail.com>
2025-01-20 19:12:29 +0900

    Complete initLoad()

--- src/init-load.js
+++ src/init-load.js
@@ -1,3 +1,19 @@
 export function initLoad(){
-    
+    const contentDiv = document.querySelector('#content');
+
+    const restaurantInfoDiv = document.createElement('div');
+    restaurantInfoDiv.classList.add('restaurant-info');
+    const restaurantInfoH1 = document.createElement('h1');
+    restaurantInfoH1.textContent = 'Est. 1997';
+    restaurantInfoDiv.appendChild(restaurantInfoH1);
+    const restaurantInfoH3 = document.createElement('h3');
+    restaurantInfoH3.textContent = 'Serving hot burgers with fresh ingredients and crispy fries';
+    restaurantInfoDiv.appendChild(restaurantInfoH3);
+
+    const homeImage = document.createElement('img');
+    homeImage.src = './images/burger-hands.jpg';
+    homeImage.alt = 'man throwing a burger up to display all the parts';
+
+    contentDiv.appendChild(restaurantInfoDiv);
+    contentDiv.appendChild(homeImage);
 }
