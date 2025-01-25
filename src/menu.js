8cab1e1829d4d27e66e80ddb11dcfd9050f35817
daniellonsdale <danielllonsdale@gmail.com>
2025-01-25 16:08:53 +0900

    Create module and import images

---
+++ src/menu.js
@@ -0,0 +1,8 @@
+import hamburgerImage from './images/hamburger.jpg';
+import cheeseburgerImage from './images/cheeseburger.jpg';
+import fullyLoadedImage from './images/fully-loaded-cheeseburger.jpg';
+import baconCheeseburgerImage from './images/bacon-cheeseburger.jpg';
+
+export function menuLoad(){
+    
+}
