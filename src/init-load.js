71dccaffefa7553d0a7299e947b2b4de75c59b46
daniellonsdale <danielllonsdale@gmail.com>
2025-01-25 15:18:25 +0900

    Increase homepage specificity

--- src/init-load.js
+++ src/init-load.js
@@ -6,15 +6,18 @@ export function initLoad(){
     const restaurantInfoDiv = document.createElement('div');
     restaurantInfoDiv.classList.add('restaurant-info');
     const restaurantInfoH1 = document.createElement('h1');
+    restaurantInfoH1.classList.add('restaurant-info-h1');
     restaurantInfoH1.textContent = 'Est. 1997';
     restaurantInfoDiv.appendChild(restaurantInfoH1);
     const restaurantInfoH3 = document.createElement('h3');
     restaurantInfoH3.textContent = 'Serving hot burgers with fresh ingredients and crispy fries';
+    restaurantInfoH3.classList.add('restaurant-info-h3');
     restaurantInfoDiv.appendChild(restaurantInfoH3);
 
     const homeImage = document.createElement('img');
     homeImage.src = homeImageSrc;
     homeImage.alt = 'man throwing a burger up to display all the parts';
+    homeImage.classList.add('home-image');
 
     contentDiv.appendChild(restaurantInfoDiv);
     contentDiv.appendChild(homeImage);
