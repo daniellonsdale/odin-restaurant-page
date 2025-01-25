e9244efb8aa04219efc4e0708360136fe6c5abf7
daniellonsdale <danielllonsdale@gmail.com>
2025-01-25 16:17:19 +0900

    Add DOM manipulation for hamburger

--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,29 @@
-import hamburgerImage from './images/hamburger.jpg';
-import cheeseburgerImage from './images/cheeseburger.jpg';
-import fullyLoadedImage from './images/fully-loaded-cheeseburger.jpg';
-import baconCheeseburgerImage from './images/bacon-cheeseburger.jpg';
+import hamburgerImageSource from './images/hamburger.jpg';
+import cheeseburgerImageSource from './images/cheeseburger.jpg';
+import fullyLoadedImageSource from './images/fully-loaded-cheeseburger.jpg';
+import baconCheeseburgerImageSource from './images/bacon-cheeseburger.jpg';
 
 export function menuLoad(){
+    const contentDiv = document.querySelector('#content');
+    const menuContainer = document.createElement('div');
+    menuContainer.classList.add('menu-container');
+
+    const hamburgerCard = document.createElement('div');
+    hamburgerCard.classList.add('menu-card');
+    hamburgerCard.classList.add('hamburger');
+    const hamburgerCardH1 = document.createElement('h1');
+    hamburgerCardH1.textContent = 'Classic Hamburger';
+    const hamburgerCardImage = document.createElement('img');
+    hamburgerCardImage.src = hamburgerImageSource;
+    hamburgerCardImage.alt = 'plain hamburger';
+    const hamburgerCardP = document.createElement('p');
+    hamburgerCardP.textContent = 'A classic hamburger with lettuce, tomato, and your choice of condiments';
+    const hamburgerCardH3 = document.createElement('h3');
+    hamburgerCardH3.textContent = '$5.99';
+    hamburgerCard.appendChild(hamburgerCardH1);
+    hamburgerCard.appendChild(hamburgerCardImage);
+    hamburgerCard.appendChild(hamburgerCardP);
+    hamburgerCard.appendChild(hamburgerCardH3);
+
     
 }
