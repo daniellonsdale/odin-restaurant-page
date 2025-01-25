71a82170417ef4117e8b09e46707f2ff27b944c6
daniellonsdale <danielllonsdale@gmail.com>
2025-01-25 16:24:09 +0900

    Add querySelectors for nav buttons

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import "./styles.css";
 import { initLoad } from "./init-load";
 import { menuLoad } from "./menu";
 
+const contentDiv = document.querySelector('#content');
+const homeBtn = document.querySelector('#home');
+const menuBtn = document.querySelector('#menu');
+const aboutBtn = document.querySelector('#about');
+
 initLoad();
